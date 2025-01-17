d87873d10b319612862e3dee1a1fbd2f1e4cbfb6
mtmiezan <miezankone@gmail.com>
2025-01-17 15:29:50 -0500

    delete wrong comments

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -12,7 +12,7 @@ function DetailsPage() {
 
 	useEffect(() => {
         const authenticationToken = sessionStorage.getItem('auth-token');
-        if (!authenticationToken) {
+       if (!authenticationToken) {
 			// Task 1: Check for authentication and redirect
             navigate('/app/login');
         }
@@ -92,7 +92,6 @@ return (
                             <div className="no-image-available-large">No Image Available</div>
                         )}
                     </div>
-                    // Task 6: Display gift details
                     	<p><strong>Category:</strong> 
 				{gift.category}
 			</p>
@@ -112,7 +111,6 @@ return (
             </div>
             <div className="comments-section mt-4">
                 <h3 className="mb-3">Comments</h3>
-				// Task 7: Render comments section by using the map function to go through all the comments
 				{comments.map((comment, index) => (
                     <div key={index} className="card mb-3">
                         <div className="card-body">
