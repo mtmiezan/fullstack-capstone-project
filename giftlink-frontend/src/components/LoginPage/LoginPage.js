46a7fa835085d7d840d8edf93e8cca9e44d7fdf2
mtmiezan <miezankone@gmail.com>
2025-01-19 13:57:24 -0500

    Integrate Api login in the front

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,13 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    // User already logged in
+    useEffetct(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            logger.info('user already logged in');
+            navigate('/app');
+        }
+    }, [navigate]);
+
 
     const handleLogin = async () => {
-        console.log("Inside handleLogin");
+        try {
+            //Step 1: Implement API call
+            const url = `${urlConfig.backendUrl}/api/auth/login`;
+            const reponse = await fectch(url, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': bearerToken ? `Bearer ${bearerToken}` : '',// Include Bearer token if available
+                },
+                body: JSON.stringify({
+                    email,
+                    password,
+                })
+            })
+            //Step 2: Access data and set user details
+            const json = await reponse.json();
+            const { authtoken, userName, userEmail } = json;
+            if (authtoken) {
+                //Set user details in session storage
+                sessionStorage.setItem('auth-token', authtoken);
+                sessionStorage.setItem('name', userName);
+                sessionStorage.setItem('emai', userEmail);
+                //Set the state of the user to logged in  on app context
+                setIsLoggedIn(true);
+                //Navigate to main page
+                navigate('/app');
+            }
+            else {
+                document.getElementById("email").value = "";
+                document.getElementById("password").value = "";
+                logger.error('Wrong password. Try again.');
+                setIncorrect('Wrong password. Try again.');
+                // Clear incorrect message after 2 secondes
+                setTimeout(() => {
+                    setIncorrect("");
+                }, 2000);
+            }
+        }
+        catch {
+
+        }
     }
+
     return (
         <div className="container mt-5">
             <div className="row justify-content-center">
@@ -26,7 +84,19 @@ function LoginPage() {
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
-                        
+
+                        <div className="mb-4">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input
+                                id="password"
+                                type="password"
+                                className="form-control"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
+                            />
+                            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
+                        </div>
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
                         <p className="mt-4 text-center">
                             New here? <a href="/app/register" className="text-primary">Register Here</a>
